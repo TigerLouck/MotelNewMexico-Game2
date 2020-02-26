95a5baf30babeee27d2e7884ab41da1e23660aaf
TigerLouck <tigerlouck@gmail.com>
2020-02-25 22:59:49 -0500

    Fixed some math in spline object generator

--- Assets/Scripts/NewGen.cs
+++ Assets/Scripts/NewGen.cs
@@ -8,11 +8,11 @@ public class NewGen : MonoBehaviour
     private int numNodes;
     private int copiesPerShape;
     private float count = 0f;
-    private GameObject spline;
+    private Spline spline;
     public GameObject nodesScript;
     public GameObject item;
     public GameObject obstacle;
-    private Vector3 splineLocation, splineTan, splineDir;
+    private Vector3 splineLocation, splineTan, splineUp;
     private Quaternion splineRotation;
 
     // Start is called before the first frame update
@@ -20,7 +20,7 @@ public class NewGen : MonoBehaviour
     {
         copiesPerShape = 5;
         numNodes = nodesScript.GetComponent<Spline>().nodes.Count;
-        spline = GameObject.Find("Smoothed"); //the spline
+        spline = GameObject.Find("Smoothed").GetComponent<Spline>(); //the spline
         GenerateObjects();
     }
 
@@ -37,9 +37,11 @@ public class NewGen : MonoBehaviour
         while (count <= numNodes)
         {
             //move along the spline
-            splineLocation = (spline.GetComponent<SplineMesh.Spline>().GetSample(count)).location; //location tracked 2 parents up
-            splineRotation = (spline.GetComponent<SplineMesh.Spline>().GetSample(count)).Rotation; //rotation tracked 1 parent up, camera being in same level but above
-            splineTan = spline.GetComponent<Spline>().GetSample(count).tangent;
+            CurveSample sample = spline.GetSample(count);
+            splineLocation = sample.location; //location tracked 2 parents up
+            splineRotation = sample.Rotation; //rotation tracked 1 parent up, camera being in same level but above
+            splineTan = sample.tangent;
+            splineUp = sample.up;
             Ring(splineLocation);
             //splineDir = spline.GetComponent<Spline>().GetSample(count).Direction;
             //Instantiate(item, splineLocation, splineRotation);
@@ -52,13 +54,13 @@ public class NewGen : MonoBehaviour
     /// </summary>
     void Ring(Vector3 centerPosNode)
     {
-        float angle = Mathf.Deg2Rad * 360f / copiesPerShape;
+        float angle = 360f / copiesPerShape;
         float radius = 1.0f;
         for (int i = 0; i < copiesPerShape; i++)
         {
-            //Quaternion rotation = Quaternion.AngleAxis(i * angle,Vector3.up);
+            Vector3 direction = Quaternion.AngleAxis(i * angle, splineTan) * splineUp;
             //Vector3 direction = Vector3.ProjectOnPlane(Random.insideUnitCircle.normalized, splineTan);
-            Vector3 direction = Vector3.ProjectOnPlane(new Vector2(Mathf.Cos(angle * i), Mathf.Sin(angle * i)).normalized, splineTan);
+            //Vector3 direction = Vector3.ProjectOnPlane(new Vector2(Mathf.Cos(angle * i), Mathf.Sin(angle * i)).normalized, splineTan);
 
             Vector3 position = centerPosNode + (direction * radius);
             Instantiate(item, position, splineRotation);//Quaternion.Euler(splineTan));
