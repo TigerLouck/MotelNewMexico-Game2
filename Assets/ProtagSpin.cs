717d086662dc687eedeca6fd08d2a7dd3f969785
Tiger Louck <tigerlouck@gmail.com>
2020-02-28 09:59:07 -0500

    Working on protag spinny

---
+++ Assets/ProtagSpin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProtagSpin : MonoBehaviour
+{
+    Vector3 rotaryInertia;
+    // Update is called once per frame
+    void Update()
+    {
+        rotaryInertia = Quaternion.AngleAxis(0, Random.onUnitSphere) * rotaryInertia;
+        //transform.rotation = rotaryInertia *
+    }
+}
